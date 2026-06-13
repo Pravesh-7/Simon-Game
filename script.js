07f42a0738c818e8d66fd15b9338e894130eb5dd
Pravesh-7 <praveshyadav9929@gmail.com>
2026-06-13 12:25:59 +0530

    Just  writing the code

---
+++ script.js
@@ -0,0 +1,23 @@
+let gameSeq = [];
+let userSeq=[];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress" , function(){
+    if(started ==false){
+      console.log("Game started");
+      started  =true;
+
+      leveUp();
+    }
+    
+});
+
+function levelUp(){
+    level++;
+   
+}
+
